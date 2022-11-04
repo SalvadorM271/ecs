005dfcb4d0768fbcdb5f60a2bbf8aa78ba3fc06c
salvadorM271 <guardianes27@outlook.com>
2022-11-03 23:16:18 -0600

    cleaning up

--- main.tf
+++ main.tf
@@ -332,7 +332,7 @@ module "autoscaling" {
     cpu_target_value = var.cpu_target_value
 }
 
-//cloudflare------------------------------------------------add variables below this to other env
+//cloudflare-----------------------------------------------add variables below this to other env
 
 provider "cloudflare" {
   email = var.cloudflare_email
