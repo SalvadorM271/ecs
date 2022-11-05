0a6a32ec649b7f8fdd9747fad36dffeefbfc4684
salvadorM271 <guardianes27@outlook.com>
2022-11-04 22:30:08 -0600

    added diagram

--- main.tf
+++ main.tf
@@ -24,37 +24,7 @@ provider "aws" {
 }
 
 
-//cloudwatch alarms-----------------------------------
 
-resource "aws_sns_topic" "xxxx_cloudwatch_notifications" {
-  name = "xxxx_cloudwatch_notifications"
-}
-
-resource "aws_sns_topic_subscription" "xxxx_cloudwatch_notifications" {
-    topic_arn = "${aws_sns_topic.xxxx_cloudwatch_notifications.arn}"
-    protocol  = "email"
-    endpoint  = "leandromenendez971@gmail.com"
-}
-
-resource "aws_cloudwatch_metric_alarm" "high_cpu_policy_alarm" {
-
-  alarm_name          = "high-cpu-ecs-service"
-  alarm_description   = "High CPUPolicy Landing Page for ecs service"
-  comparison_operator = "GreaterThanOrEqualToThreshold"
-  evaluation_periods  = "2"
-  metric_name         = "CPUUtilization"
-  namespace           = "AWS/ECS"
-  period              = "60"
-  statistic           = "Maximum"
-  threshold           = 1
-  treat_missing_data = "notBreaching"
-  alarm_actions     = [aws_sns_topic.xxxx_cloudwatch_notifications.arn]
-
-  dimensions = {
-    "ServiceName" = module.esc_cluster.myService.name
-    "ClusterName" = module.esc_cluster.myECS.id
-  }
-}
 
 
 
