cacf13ce8c674bcea7d4bab733dd6d41ccd273e2
salvadorM271 <guardianes27@outlook.com>
2022-11-04 20:54:11 -0600

    test

--- README.md
+++ README.md
@@ -65,4 +65,4 @@ and finnaly
 
 ## Diagram
 
-<img src="./images/ecs.drawio.svg" width="450"/>
+<img src="./images/ecs.drawio.svg" width="850"/>
--- main.tf
+++ main.tf
@@ -24,6 +24,37 @@ provider "aws" {
 }
 
 
+//cloudwatch alarms-----------------------------------
+
+resource "aws_sns_topic" "xxxx_cloudwatch_notifications" {
+  name = "xxxx_cloudwatch_notifications"
+}
+
+resource "aws_sns_topic_subscription" "xxxx_cloudwatch_notifications" {
+    topic_arn = "${aws_sns_topic.xxxx_cloudwatch_notifications.arn}"
+    protocol  = "email"
+    endpoint  = "caravana271@gmail.com"
+}
+
+resource "aws_cloudwatch_metric_alarm" "high_cpu_policy_alarm" {
+
+  alarm_name          = "high-cpu-ecs-service"
+  alarm_description   = "High CPUPolicy Landing Page for ecs service"
+  comparison_operator = "GreaterThanOrEqualToThreshold"
+  evaluation_periods  = "2"
+  metric_name         = "CPUUtilization"
+  namespace           = "AWS/ECS"
+  period              = "60"
+  statistic           = "Maximum"
+  threshold           = 1
+
+  dimensions = {
+    "ServiceName" = module.esc_cluster.myService.name
+    "ClusterName" = module.esc_cluster.myECS.id
+  }
+}
+
+
 
 
 
